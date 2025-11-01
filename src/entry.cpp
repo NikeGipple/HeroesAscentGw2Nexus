6289760b974c9d06b8bcd1bfa2f88443a7e3675a
NikeGipple <NikeGipple>
2025-11-01 21:54:48 +0100

    primo commit

--- src/entry.cpp
+++ src/entry.cpp
@@ -1,149 +1,120 @@
-///----------------------------------------------------------------------------------------------------
-/// Copyright (c) Raidcore.GG - All rights reserved.
-///
-/// This code is licensed under the MIT license.
-/// You should have received a copy of the license along with this source file.
-/// You may obtain a copy of the license at: https://opensource.org/license/MIT
-/// 
-/// Name         :  entry.cpp
-/// Description  :  Simple example of a Nexus addon implementation.
-///----------------------------------------------------------------------------------------------------
-
-#include <Windows.h>
-
+﻿#include <Windows.h>
+#include <string>
 #include "nexus/Nexus.h"
 #include "mumble/Mumble.h"
 #include "imgui/imgui.h"
 
-/* proto */
+/* Prototipi */
 void AddonLoad(AddonAPI* aApi);
 void AddonUnload();
 void AddonRender();
-void AddonOptions();
 
-/* globals */
+/* Globali */
 AddonDefinition AddonDef = {};
-HMODULE hSelf            = nullptr;
-AddonAPI* APIDefs        = nullptr;
-NexusLinkData* NexusLink = nullptr;
+AddonAPI* APIDefs = nullptr;
 Mumble::Data* MumbleLink = nullptr;
+NexusLinkData* NexusLink = nullptr;
+HMODULE hSelf = nullptr;
 
-bool someSetting         = false;
-
-///----------------------------------------------------------------------------------------------------
-/// DllMain:
-/// 	Main entry point for DLL.
-/// 	We are not interested in this, all we get is our own HMODULE in case we need it.
-///----------------------------------------------------------------------------------------------------
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
-{
-	switch (ul_reason_for_call)
-	{
-		case DLL_PROCESS_ATTACH: hSelf = hModule; break;
-		case DLL_PROCESS_DETACH: break;
-		case DLL_THREAD_ATTACH: break;
-		case DLL_THREAD_DETACH: break;
-	}
-	return TRUE;
+/* Entry Point DLL */
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID lpReserved) {
+    if (reason == DLL_PROCESS_ATTACH) hSelf = hModule;
+    return TRUE;
 }
 
-///----------------------------------------------------------------------------------------------------
-/// GetAddonDef:
-/// 	Export needed to give Nexus information about the addon.
-///----------------------------------------------------------------------------------------------------
-extern "C" __declspec(dllexport) AddonDefinition* GetAddonDef()
-{
-	AddonDef.Signature = -12345; // set to random unused negative integer
-	AddonDef.APIVersion = NEXUS_API_VERSION;
-	AddonDef.Name = "My First Nexus Addon";
-	AddonDef.Version.Major = 1;
-	AddonDef.Version.Minor = 0;
-	AddonDef.Version.Build = 0;
-	AddonDef.Version.Revision = 1;
-	AddonDef.Author = "Me, Myself and I";
-	AddonDef.Description = "This is my first Nexus addon.";
-	AddonDef.Load = AddonLoad;
-	AddonDef.Unload = AddonUnload;
-	AddonDef.Flags = EAddonFlags_None;
-
-	/* not necessary if hosted on Raidcore, but shown anyway for the example also useful as a backup resource */
-	//AddonDef.Provider = EUpdateProvider_GitHub;
-	//AddonDef.UpdateLink = "https://github.com/RaidcoreGG/GW2Nexus-AddonTemplate";
-
-	return &AddonDef;
+/* Definizione Addon */
+extern "C" __declspec(dllexport) AddonDefinition* GetAddonDef() {
+    AddonDef.Signature = -987654321;
+    AddonDef.APIVersion = NEXUS_API_VERSION;
+    AddonDef.Name = "HeroesAscentGw2Nexus";
+    AddonDef.Version.Major = 1;
+    AddonDef.Version.Minor = 3;
+    AddonDef.Version.Build = 0;
+    AddonDef.Version.Revision = 0;
+    AddonDef.Author = "Dresna & Co.";
+    AddonDef.Description = "Overlay test - mostra info Mumble e Nexus";
+    AddonDef.Load = AddonLoad;
+    AddonDef.Unload = AddonUnload;
+    AddonDef.Flags = EAddonFlags_None;
+    return &AddonDef;
 }
 
-///----------------------------------------------------------------------------------------------------
-/// AddonLoad:
-/// 	Load function for the addon, will receive a pointer to the API.
-/// 	(You probably want to store it.)
-///----------------------------------------------------------------------------------------------------
-void AddonLoad(AddonAPI* aApi)
-{
-	APIDefs = aApi; // store the api somewhere easily accessible
+/* Addon Load */
+void AddonLoad(AddonAPI* aApi) {
+    APIDefs = aApi;
 
-	ImGui::SetCurrentContext((ImGuiContext*)APIDefs->ImguiContext); // cast to ImGuiContext*
-	ImGui::SetAllocatorFunctions((void* (*)(size_t, void*))APIDefs->ImguiMalloc, (void(*)(void*, void*))APIDefs->ImguiFree); // on imgui 1.80+
+    ImGui::SetCurrentContext((ImGuiContext*)APIDefs->ImguiContext);
+    ImGui::SetAllocatorFunctions(
+        (void* (*)(size_t, void*))APIDefs->ImguiMalloc,
+        (void(*)(void*, void*))APIDefs->ImguiFree);
 
-	NexusLink = (NexusLinkData*)APIDefs->DataLink.Get("DL_NEXUS_LINK");
-	MumbleLink = (Mumble::Data*)APIDefs->DataLink.Get("DL_MUMBLE_LINK");
+    MumbleLink = (Mumble::Data*)APIDefs->DataLink.Get("DL_MUMBLE_LINK");
+    NexusLink = (NexusLinkData*)APIDefs->DataLink.Get("DL_NEXUS_LINK");
 
-	// Add an options window and a regular render callback
-	APIDefs->Renderer.Register(ERenderType_Render, AddonRender);
-	APIDefs->Renderer.Register(ERenderType_OptionsRender, AddonOptions);
+    // Registriamo il renderer subito
+    APIDefs->Renderer.Register(ERenderType_Render, AddonRender);
+    APIDefs->Log(ELogLevel_INFO, "HeroesAscent", "Renderer registrato immediatamente.");
 
-	APIDefs->Log(ELogLevel_DEBUG, "My First addon", "My <c=#00ff00>first addon</c> was loaded.");
+    APIDefs->Log(ELogLevel_INFO, "HeroesAscent", "AddonLoad completato - pronto a mostrare overlay.");
 }
 
-///----------------------------------------------------------------------------------------------------
-/// AddonUnload:
-/// 	Everything you registered in AddonLoad, you should "undo" here.
-///----------------------------------------------------------------------------------------------------
-void AddonUnload()
-{
-	/* let's clean up after ourselves */
-	APIDefs->Renderer.Deregister(AddonRender);
-	APIDefs->Renderer.Deregister(AddonOptions);
-
-	APIDefs->Log(ELogLevel_DEBUG, "My First addon", "<c=#ff0000>Signing off</c>, it was an honor commander.");
+/* Addon Unload */
+void AddonUnload() {
+    if (APIDefs)
+        APIDefs->Renderer.Deregister(AddonRender);
+    APIDefs->Log(ELogLevel_INFO, "HeroesAscent", "Addon scaricato correttamente.");
 }
 
-///----------------------------------------------------------------------------------------------------
-/// AddonRender:
-/// 	Called every frame. Safe to render any ImGui.
-/// 	You can control visibility on loading screens with NexusLink->IsGameplay.
-///----------------------------------------------------------------------------------------------------
-void AddonRender()
-{
-	ImGuiIO& io = ImGui::GetIO();
+/* Render principale */
+void AddonRender() {
+    if (!APIDefs)
+        return;
 
-	if (ImGui::Begin("MyFirstImGuiWindow"))
-	{
-		ImGui::Text("Hello Tyria!");
-		ImGui::Text("UI Tick: %u",
-			nullptr != MumbleLink
-			? MumbleLink->UITick
-			: 0
-		);
+    ImGui::SetNextWindowPos(ImVec2(20, 20), ImGuiCond_FirstUseEver);
+    ImGui::SetNextWindowSize(ImVec2(500, 260), ImGuiCond_FirstUseEver);
 
-		ImGui::Text("%s",
-			nullptr != NexusLink
-			? NexusLink->IsMoving
-				? "Currently moving!"
-				: "Currently standing still."
-			: "We don't know whether we are standing or moving? NexusLink seems to be empty."
-		);
-	}
-	ImGui::End();
-}
+    ImGui::Begin("HeroesAscent Overlay", nullptr,
+        ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_AlwaysAutoResize |
+        ImGuiWindowFlags_NoSavedSettings);
 
-///----------------------------------------------------------------------------------------------------
-/// AddonOptions:
-/// 	Basically an ImGui callback that doesn't need its own Begin/End calls.
-///----------------------------------------------------------------------------------------------------
-void AddonOptions()
-{
-	ImGui::Separator();
-	ImGui::Text("My first Nexus addon");
-	ImGui::Checkbox("Some setting", &someSetting);
+    std::string playerName = "Nessun nome rilevato";
+    std::string rawJson = "(vuoto)";
+
+    if (MumbleLink && MumbleLink->Identity && wcslen(MumbleLink->Identity) > 0) {
+        // Convertiamo il JSON grezzo
+        char jsonUtf8[2048];
+        WideCharToMultiByte(CP_UTF8, 0, MumbleLink->Identity, -1, jsonUtf8, sizeof(jsonUtf8), nullptr, nullptr);
+        rawJson = jsonUtf8;
+
+        // Estrazione semplice del nome
+        const wchar_t* p = wcsstr(MumbleLink->Identity, L"\"name\":\"");
+        if (p) {
+            p += 8;
+            const wchar_t* end = wcschr(p, L'"');
+            if (end) {
+                std::wstring wName(p, end - p);
+                char nameUtf8[256];
+                WideCharToMultiByte(CP_UTF8, 0, wName.c_str(), -1, nameUtf8, sizeof(nameUtf8), nullptr, nullptr);
+                playerName = nameUtf8;
+            }
+        }
+    }
+
+    ImGui::Text("HeroesAscent Overlay attivo");
+    ImGui::Separator();
+    ImGui::Text("Nome del personaggio: %s", playerName.c_str());
+    ImGui::Text("Movimento: %s", (NexusLink && NexusLink->IsMoving) ? "In movimento" : "Fermo");
+
+    if (MumbleLink) {
+        const float* pos = reinterpret_cast<const float*>(&MumbleLink->AvatarPosition);
+        ImGui::Text("Posizione: X %.2f | Y %.2f | Z %.2f", pos[0], pos[1], pos[2]);
+    }
+
+    ImGui::Separator();
+    ImGui::Text("Raw Mumble JSON:");
+    ImGui::PushTextWrapPos(ImGui::GetCursorPos().x + 460);
+    ImGui::TextWrapped("%s", rawJson.c_str());
+    ImGui::PopTextWrapPos();
+
+    ImGui::End();
 }
